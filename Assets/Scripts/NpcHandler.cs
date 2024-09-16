f4c771e8c5ba16e555262b7ff323d6062d27a8b1
Axyu01 <adriansosniak01@gmail.com>
2024-09-16 21:30:58 +0200

    Revert "Added simple interactive menu"
    This reverts commit 34f81fe319c7d7236c8667e72a572577bf35db63.

--- Assets/Scripts/NpcHandler.cs
+++ Assets/Scripts/NpcHandler.cs
@@ -3,20 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NpcHandler : MonoBehaviour
-{
-    [SerializeField]
-    GameObject ShopUi;
-    private bool _isShopActive;
+{ 
     public void Interaction()
     {
         Debug.Log("witam");
-    }
-
-    public void Shop()
-    {
-        if (_isShopActive == false)
-        {
-            ShopUi.SetActive(true);
-        }
     }
 }
