0a7739907525f95e5f66aae220580c5941dac834
Bartek <97710444+kieronsky@users.noreply.github.com>
2024-09-16 03:01:29 +0200

    prototyping(read description)
    -added walkable surface for dynamic navmesh
    -added obstacles
    -added player with movement input
    -added npc with working interaction on E
    -added enemies with player following, finding new path

---
+++ Assets/Scripts/NpcHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcHandler : MonoBehaviour
+{
+    
+    public void Interaction()
+    {
+        Debug.Log("witam");
+    }
+}
