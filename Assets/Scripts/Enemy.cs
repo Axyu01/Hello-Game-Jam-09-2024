da679797cff08a6d3f0a9eb3e2a03b08a06b9318
Axyu01 <adriansosniak01@gmail.com>
2024-09-16 13:35:21 +0200

    made world switching prototype

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,19 +6,25 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] Transform target;
-
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float force = 5f;
     NavMeshAgent agent;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();   
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        agent.updatePosition = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         agent.SetDestination(target.position);
-        
+        if (agent.path.corners.Length > 0)
+        {
+            rb.AddForce(((Vector2)(agent.path.corners[0] - transform.position)).normalized * force,ForceMode2D.Force);
+        }
+        agent.nextPosition = transform.position;
     }
 }
---
+++ Assets/Scripts/Sosivo_tests/WorldSwitcher.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    KeyCode switchKey = KeyCode.P;
+    [SerializeField]
+    GameObject _chillWorld;
+    [SerializeField]
+    GameObject _actionWorld;
+
+    [SerializeField]
+    bool _isChillWorldActive = false;
+    bool _isChillWorldActiveLastValue = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateWorlds();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_isChillWorldActive != _isChillWorldActiveLastValue)
+        {
+            UpdateWorlds();
+            _isChillWorldActiveLastValue = _isChillWorldActive;
+        }
+        if(Input.GetKeyDown(switchKey))
+        {
+            _isChillWorldActive = !_isChillWorldActive;
+            UpdateWorlds();
+            _isChillWorldActiveLastValue = _isChillWorldActive;
+        }
+    }
+    void UpdateWorlds()
+    {
+        if(_isChillWorldActive)
+        {
+            _chillWorld.SetActive(true);
+            _actionWorld.SetActive(false);
+        }
+        else
+        {
+            _chillWorld.SetActive(false);
+            _actionWorld.SetActive(true);
+        }
+    }
+}
