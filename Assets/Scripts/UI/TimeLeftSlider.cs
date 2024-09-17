ca4158a96a919350698472d0a9eebaeb537ad72d
Bartek <97710444+kieronsky@users.noreply.github.com>
2024-09-18 01:44:52 +0200

    fixing slider to make it disappear after wave end

--- Assets/Scripts/UI/TimeLeftSlider.cs
+++ Assets/Scripts/UI/TimeLeftSlider.cs
@@ -14,6 +14,9 @@ public class TimeLeftSlider : MonoBehaviour
         {
             timeSlider.maxValue = waveManager.TimePerWave;
             timeSlider.value = waveManager.TimePerWave;
+
+            waveManager.WaveEndEvent.AddListener(HideSlider);
+            waveManager.WaveStartEvent.AddListener(ShowSlider);
         }
     }
 
@@ -22,6 +25,26 @@ public class TimeLeftSlider : MonoBehaviour
         if (waveManager != null && timeSlider != null)
         {
             timeSlider.value = waveManager.TimeLeft;
+        }
+    }
+
+    void HideSlider()
+    {
+        timeSlider.gameObject.SetActive(false);
+    }
+
+    void ShowSlider()
+    {
+        timeSlider.gameObject.SetActive(true);
+        timeSlider.value = waveManager.TimePerWave;
+    }
+
+    private void OnDestroy()
+    {
+        if (waveManager != null)
+        {
+            waveManager.WaveEndEvent.RemoveListener(HideSlider);
+            waveManager.WaveStartEvent.RemoveListener(ShowSlider);
         }
     }
 }
