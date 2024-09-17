c5e530a0754cb3b6a54f9206ceea06901088235e
Bartek <97710444+kieronsky@users.noreply.github.com>
2024-09-17 22:51:59 +0200

    added timeleft slider in UI

---
+++ Assets/Scripts/UI/TimeLeftSlider.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeLeftSlider : MonoBehaviour
+{
+    public Slider timeSlider;
+    public WaveManager waveManager;
+
+    private void Start()
+    {
+        if (waveManager != null && timeSlider != null)
+        {
+            timeSlider.maxValue = waveManager.TimePerWave;
+            timeSlider.value = waveManager.TimePerWave;
+        }
+    }
+
+    private void Update()
+    {
+        if (waveManager != null && timeSlider != null)
+        {
+            timeSlider.value = waveManager.TimeLeft;
+        }
+    }
+}
