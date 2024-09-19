a06037bf36af9dfce8c9e304f3cb4c4c60676428
Bartek <97710444+kieronsky@users.noreply.github.com>
2024-09-19 03:44:36 +0200

    added coin display for ui and testing crosshair

---
+++ Assets/Scripts/UI/UI_CrosshairDisplay.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crosshair : MonoBehaviour
+{
+    public RectTransform CrosshairImage;
+    private Canvas _canvas;
+
+    void Start()
+    {
+        _canvas = GetComponentInParent<Canvas>();
+        //Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(_canvas.transform as RectTransform, mousePosition, _canvas.worldCamera, out Vector2 localPoint);
+        CrosshairImage.localPosition = localPoint;
+    }
+}
